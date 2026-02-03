27690a2980d69bb8d7c721d9dd1c4887a927a0b1
AbhiroopGoel <157642305+AbhiroopGoel@users.noreply.github.com>
2026-02-03 15:46:11 -0600

    Add Login component for user authentication

---
+++ teamhub-frontend/src/pages/login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold">Login</h1>
+      <p>Please sign in to continue.</p>
+    </div>
+  );
+};
+
+export default Login;
