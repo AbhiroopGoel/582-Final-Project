e3c2802da09c6ab2e3081e57a9bc0dfae993ab53
AbhiroopGoel <157642305+AbhiroopGoel@users.noreply.github.com>
2026-02-15 21:47:24 +0000

    added

--- teamhub-frontend/src/components/ProjectedRoute.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-import { isAuthed } from "../lib/auth";
-
-export default function ProtectedRoute({ children }) {
-  if (!isAuthed()) return <Navigate to="/login" replace />;
-  return children;
-}
